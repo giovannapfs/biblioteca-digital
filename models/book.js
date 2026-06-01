854b3a15d6c820bf6b94751ce8ad33275eca2e02
giovannapfs <giovanna.figueirasilva@gmail.com>
2026-06-01 13:54:31 -0300

    Aula 6

--- models/book.js
+++ models/book.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
-   title: { type: String }, // Título do livro
-   author: { type: String }, // Autor
-   year: { type: Number }, // Ano de publicação
-   genre: { type: String },  // Gênero
-   createdAt: { type: Date, default: Date.now }  // Data de criação
+ title: { type: String, required: [true, "O título é obrigatório"], minlength: [3, "O título deve ter pelo menos 3 caracteres"] },
+ author: { type: String, required: [true, "O autor é obrigatório"], minlength: [3, "O nome do autor deve ter pelo menos 3 caracteres"] },
+ year: { type: Number, required: [true, "O ano de publicação é obrigatório"], min: [1000, "Ano inválido"], max: [new Date().getFullYear(), "O ano não pode ser no futuro"] },
+ genre: { type: String, required: [true, "O gênero é obrigatório"] },
+ createdAt: { type: Date, default: Date.now },
 });
 
-
 const Book = mongoose.model('Book', bookSchema);
-
 
 module.exports = Book;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/config");
 
+
 const Book = require("./models/book");
+const validateTitle = require("./middlewares/validateTitle");
 
 const app = express();
 
@@ -9,7 +11,7 @@ connectDB();
 app.use(express.json());
 
 //adicionando um novo livro
-app.post("/api/books", async (req, res) => {
+app.post("/api/books", validateTitle, async (req, res) => {
  try {
    const { title, author, year, genre } = req.body;
    const newBook = new Book({ title, author, year, genre });
@@ -31,21 +33,55 @@ app.get("/api/books", async (_req, res) => {
 });
 
 //buscando um livro pelo id, a busca vai ficar /api/books/numeroDoId 
-app.get("/api/books/:id", async (req, res) => {
+app.get("/api/books/:id", async (req, res, next) => {
  try {
    const { id } = req.params;
    const book = await Book.findById(id);
-
-
    if (!book) {
      return res.status(404).json({ error: "Livro não encontrado" });
    }
-
-
    res.json(book);
  } catch (err) {
-   res.status(500).json({ error: "Erro ao buscar livro" });
+   next(err); // Passa o erro para o middleware de tratamento
  }
+});
+
+//atualizando um livro
+app.patch("/api/books/:id", async (req, res) => {
+ try {
+   const { id } = req.params;
+   const updates = req.body;
+   const options = { new: true, runValidators: true };
+
+
+   const updatedBook = await Book.findByIdAndUpdate(id, updates, options);
+   if (!updatedBook)
+     return res.status(404).json({ error: "Livro não encontrado" });
+
+
+   res.json(updatedBook);
+ } catch (err) {
+   res.status(500).json({ error: "Erro ao atualizar livro." });
+ }
+});
+
+//deletando um novo livro
+app.delete("/api/books/:id", async (req, res) => {
+ try {
+   const { id } = req.params;
+   const deletedBook = await Book.findByIdAndDelete(id);
+   if (!deletedBook)
+     return res.status(404).json({ error: "Livro não encontrado" });
+   res.json({ message: "Livro excluído com sucesso" });
+ } catch (err) {
+   res.status(500).json({ error: "Erro ao excluir livro" });
+ }
+});
+
+// Middleware global de erros
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Ocorreu um erro no servidor." });
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
